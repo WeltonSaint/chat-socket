ac746359747a27f55b69cae4c4477f508613f5f0
WeltonSaint <WeltonSaint@WS>
2017-04-21 23:45:03 -0300

    Third Commit

--- index.js
+++ index.js
@@ -12,17 +12,30 @@ server.listen(port)
 console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+console.log("websocket server created");
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+// Create a "broadcast" function on our WebSocketServer object.
+// The function will take a "msg" paramter. When called, it will
+// loop through all the connected clients and send them the msg.
+wss.broadcast = function broadcastMsg(msg) {
+    wss.clients.forEach(function each(client) {
+        client.send(msg);
+    });
+};
 
-  console.log("websocket connection open")
+// Create a listener function for the "connection" event.
+// Each time we get a connection, the following function
+// is called.
+wss.on('connection', function connection(ws) {
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+    // Store the remote systems IP address as "remoteIp".
+    var remoteIp = ws.upgradeReq.connection.remoteAddress;
+
+    // Print a log with the IP of the client that connected.
+    console.log('Connection received: ', remoteIp);
+
+    // Add a listener which listens for the "message" event.
+    // When a "message" event is received, take the contents
+    // of the message and pass it to the broadcast() function.
+    ws.on('message', wss.broadcast);
+});
