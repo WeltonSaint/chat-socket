9c72632c8e51c091ff16eaa92d9ba78caebdabae
WeltonSaint <WeltonSaint@WS>
2017-04-22 01:09:01 -0300

    What Commit

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+// Initialize collapse button
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+);    
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+$('.collapsible').collapsible();
